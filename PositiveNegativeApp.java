f4ac627de7638a67705e72d214fd33f97fb63789
Hanna Szczyriec <sisyrinchium02@gmail.com>
2019-10-13 16:29:17 +0300

    fix task PositiveNegativeApp

--- PositiveNegativeApp.java
+++ PositiveNegativeApp.java
@@ -2,10 +2,9 @@ public class PositiveNegativeApp {
     public static void main(String[] agrs) {
         int number1 = 40;
 
-        if (number1 > 0) {
+        if (number1 >= 0) {
             number1++;
-        }
-        if (number1 < 0) {
+        } else if (number1 < 0) {
             number1 = number1 - 2;
         } else {
             number1 = 10;
