8ad76fa5ce079e571f0da6a393cc2f91bdaeaad0
Hanna Szczyriec <sisyrinchium02@gmail.com>
2019-10-31 20:40:21 +0300

    some corrections by code (not carefully read task)

--- src/main/java/com/teachmeskill/hometask4/RandomMatrixApp.java
+++ src/main/java/com/teachmeskill/hometask4/RandomMatrixApp.java
@@ -3,33 +3,36 @@ package com.teachmeskill.hometask4;
 public class RandomMatrixApp {
     public static void main(String[] args) {
         int[][] myMatrix = new int[5][5];
-        int sumMatrixMainDiagonal = 0;
+        int sumEvenElementsMatrix = 0;
         matrixFilling(myMatrix);
-        matrixDiagonalAmount(myMatrix, sumMatrixMainDiagonal);
-        oddElements(myMatrix);
+        oddElementsMatrixDiagonal(myMatrix);
+        sumEvenElements(myMatrix, sumEvenElementsMatrix);
     }
 
-    private static void oddElements(int[][] myMatrix) {
+    private static void sumEvenElements(int[][] myMatrix, int sumEvenElementMatrix) {
         for (int i = 0; i < myMatrix.length; i++) {
             for (int j = 0; j < myMatrix[i].length; j++) {
                 if (i == j) {
-                    if (myMatrix[i][j] % 2 != 0) {
+                    if (myMatrix[i][j] % 2 == 0) {
+                        sumEvenElementMatrix += myMatrix[i][j];
                         System.out.print(myMatrix[i][j] + " ");
                     }
                 }
             }
         }
+        System.out.println(sumEvenElementMatrix);
     }
 
-    private static void matrixDiagonalAmount(int[][] myMatrix, int sumMatrixMainDiagonal) {
+    private static void oddElementsMatrixDiagonal(int[][] myMatrix) {
         for (int i = 0; i < myMatrix.length; ++i) {
             for (int j = 0; j < myMatrix[i].length; ++j) {
                 if (i == j) {
-                    sumMatrixMainDiagonal += myMatrix[i][j];
+                    if(myMatrix[i][j] % 2 !=0){
+                        System.out.println(myMatrix[i][j]);
+                    }
                 }
             }
         }
-        System.out.println(sumMatrixMainDiagonal);
     }
 
     private static void matrixFilling(int[][] myMatrix) {
